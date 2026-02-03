6e7ab9752eb055c4695be57f28c3cd17b35fba8e
jihyeonjeong11 <wjdwlgus11@gmail.com>
2026-02-03 19:54:16 +0900

    server monorepo and first test

--- apps/server/src/tests/server.test.ts
+++ apps/server/src/tests/server.test.ts
@@ -4,14 +4,12 @@ import app from '../index.js';
 
 describe('Express Server Test', () => {
   let server: Server;
-  const port = 3001; // 테스트용 포트
+  const port = 3001;
 
-  // 테스트 시작 전 서버 실행
   beforeAll(() => {
     server = app.listen(port);
   });
 
-  // 테스트 종료 후 서버 종료 (매우 중요: 안 하면 포트가 계속 열려있음)
   afterAll(() => {
     server.close();
   });
